48695a6a0e8d9f705fcad44ca46e91f2604d95e2
Pav T <pav@Pavs-MacBook-Pro.local>
2013-12-24 09:15:25 -0500

    represent image as grap

--- kruskal.py
+++ kruskal.py
@@ -1,3 +1,5 @@
+from PIL import Image, ImageFilter
+
 parent = dict()
 rank = dict()
 
@@ -35,20 +37,38 @@ def kruskal(graph):
     return minimum_spanning_tree
 
 graph = {
-        'vertices': ['A', 'B', 'C', 'D', 'E', 'F'],
-        'edges': set([
-            (1, 'A', 'B'),
-            (5, 'A', 'C'),
-            (3, 'A', 'D'),
-            (4, 'B', 'C'),
-            (2, 'B', 'D'),
-            (1, 'C', 'D'),
-            ])
+        'vertices': [],
+        'edges': set([])
         }
-minimum_spanning_tree = set([
-            (1, 'A', 'B'),
-            (2, 'B', 'D'),
-            (1, 'C', 'D'),
-            ])
 
-print(kruskal(graph) == minimum_spanning_tree)
+#print(kruskal(graph) == minimum_spanning_tree)
+
+try:
+    original = Image.open("Lenna.png")
+except:
+    print "Unable to load image"
+
+width,height = original.size
+for i in range(0,width):
+    for j in range(0,height):
+        r, g, b = original.getpixel((i, j))
+        n = r*(pow(256,2)) + g*256 + b # Convert RGB to an integer representation
+        graph['vertices'].append(str(n)) #str((i*width)+j) # Add RBG integer value of pixel to the graph as a vertex
+        #add Edges to the graph that represent differences between pixels
+        if(i>0):
+            v = graph['vertex'][((i-1)*width)+j]
+            graph['edges'].add((abs(n-int(v)),str(n),v))
+        if(i<width-1):
+            v = graph['vertex'][((i+1)*width)+j]
+            graph['edges'].add((abs(n-int(v)),str(n),v))
+        if(j>0):
+            v = graph['vertex'][(i*width)+(j-1)]
+            graph['edges'].add((abs(n-int(v)),str(n),v))
+        if(j<height-1):
+            v = graph['vertex'][(i*width)+(j+1)]
+            graph['edges'].add((abs(n-int(v)),str(n),v))
+        
+
+
+
+
